a23c8eb4a138ecb7f0d85a777f3f74dc21c6963c
Jesse Brown <Brownjesse8895@yahoo.com>
2021-12-31 14:42:55 -0600

    initial fetch/routine state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [routines, setRoutines] = useState({});
+ 
+  useEffect(() => {
+    fetchRoutines()
+  }, [])
+  
+  function fetchRoutines() {
+    fetch("http://localhost:3000/routines")
+    .then(resp=> resp.json())
+    .then(data => setRoutines(data))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+     
     </div>
   );
 }
